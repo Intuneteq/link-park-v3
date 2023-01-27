6bead1601bd4605df853cd642455e4e1a397331c
Tobi Olanitori <tobiolanitori@gmail.com>
2023-01-27 11:45:12 +0100

    fix & style: changed selector in top bar

--- src/components/molecules/selector/Selector.jsx
+++ src/components/molecules/selector/Selector.jsx
@@ -3,17 +3,30 @@ import { GrDown } from "react-icons/gr";
 
 import styles from "./selector.module.scss";
 
-const Selector = ({ title, options, width, height, padding, selectWidth }) => {
+const Selector = ({
+  borderNone,
+  title,
+  options,
+  width,
+  height,
+  padding,
+  selectWidth,
+}) => {
   const [toggle, setToggle] = useState(false);
   return (
     <div style={{ width }} className={styles.selector}>
-      <p className="p-text">{title}</p>
+      {title && <p className="p-text">{title}</p>}
       <div
         className={styles.selectMenu}
-        style={{ width: selectWidth, height, padding }}
+        style={{
+          width: selectWidth,
+          height,
+          padding,
+          borderStyle: borderNone && "none",
+        }}
       >
         <div onClick={() => setToggle(!toggle)} className={styles.selectBtn}>
-          <span className={styles.sBtnText}>{title}</span>
+          <span className={styles.sBtnText}>{options[0]}</span>
           <GrDown className={toggle ? styles.rotateUp : ""} />
         </div>
 
--- src/components/organisms/topnav/index.jsx
+++ src/components/organisms/topnav/index.jsx
@@ -1,30 +1,36 @@
-import React from 'react'
-import { CiSearch } from 'react-icons/ci'
-import { IoMdNotificationsOutline } from 'react-icons/io'
-import { Images } from '../../../constants'
+import React from "react";
+import { CiSearch } from "react-icons/ci";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import { Images } from "../../../constants";
+import { Selector } from "../../molecules";
 
-import './topnav.scss'
+import "./topnav.scss";
 
-const TopNav = ({name}) => {
+const TopNav = ({ name }) => {
+  const options = ["Temitope Adekunle", "Temitope", "Temitope"];
   return (
     <section className="dashboard__nav app__flex-2">
-    <h3>{name}</h3>
-    <div className="dashboard__nav-search">
-      <input type="search" placeholder="Search" />
-      <CiSearch />
-    </div>
-    <IoMdNotificationsOutline />
-    <div className="dashboard__nav-selector">
-      <select name="student" id="student">
-        <option value="Temitope Adekunle">Temitope Adekunle</option>
-        <option value="Temitope Adekunle">Temitope Adekunle</option>
-      </select>
-    </div>
-    <div className="dashboard__nav-user img-size">
-      <img src={Images.user} alt="user" />
-    </div>
-  </section>
-  )
-}
+      <h3>{name}</h3>
+      <div className="dashboard__nav-search">
+        <input type="search" placeholder="Search" />
+        <CiSearch />
+      </div>
+      <IoMdNotificationsOutline />
+      <div className="dashboard__nav-selector">
+        <Selector
+          borderNone
+          options={options}
+          width={"100%"}
+          height={"100%"}
+          padding={"0"}
+          selectWidth={"100%"}
+        />
+      </div>
+      <div className="dashboard__nav-user img-size">
+        <img src={Images.user} alt="user" />
+      </div>
+    </section>
+  );
+};
 
 export default TopNav;
