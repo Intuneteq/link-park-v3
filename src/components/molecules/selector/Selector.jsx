8630e3675e2177fb2a11d7c0dd6caf057e247141
Tobi Olanitori <tobiolanitori@gmail.com>
2023-01-27 11:16:04 +0100

    feat & style: redid the selector component

--- src/components/molecules/selector/Selector.jsx
+++ src/components/molecules/selector/Selector.jsx
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { GrDown } from "react-icons/gr";
 
 import styles from "./selector.module.scss";
 
 const Selector = ({ title, options, width, height, padding, selectWidth }) => {
+  const [toggle, setToggle] = useState(false);
   return (
     <div style={{ width }} className={styles.selector}>
       <p className="p-text">{title}</p>
-      <select name="" id="" style={{ width: selectWidth, height, padding }}>
-        {options.map((item, index) => (
-          <option key={index} value="">
-            {item}
-          </option>
-        ))}
-      </select>
+      <div
+        className={styles.selectMenu}
+        style={{ width: selectWidth, height, padding }}
+      >
+        <div onClick={() => setToggle(!toggle)} className={styles.selectBtn}>
+          <span className={styles.sBtnText}>{title}</span>
+          <GrDown className={toggle ? styles.rotateUp : ""} />
+        </div>
+
+        {toggle && (
+          <ul style={{ width: selectWidth }} className={styles.options}>
+            {options.map((item, index) => (
+              <li key={index} className={styles.option}>
+                <span className={styles.optionText}>{item}</span>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
 };
