044a4756c16ce5d420a3fc35a841ef8720dce401
Tobi Olanitori <tobiolanitori@gmail.com>
2023-02-09 16:24:06 +0100

    refactor: footer redo

--- src/components/organisms/footer/index.jsx
+++ src/components/organisms/footer/index.jsx
@@ -6,33 +6,76 @@ import {
   AiFillLinkedin,
 } from 'react-icons/ai'
 
-import './footer.scss'
+import styles from './footer.module.scss'
 import { Images } from '../../../constants'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
+  const quickLinks = [
+    {
+      name: 'Home',
+      path: '/',
+    },
+    {
+      name: 'About Us',
+      path: '/about',
+    },
+    {
+      name: 'Parents',
+      path: '/parents',
+    },
+  ]
+
+  const help = [
+    {
+      name: 'FAQ',
+      path: '/faq',
+    },
+    {
+      name: 'Contact Us',
+      path: '/',
+    },
+    {
+      name: 'Terms and Condition',
+      path: '/',
+    },
+    {
+      name: 'Privacy Policy',
+      path: '/',
+    },
+  ]
   return (
     <div>
-      <footer className='footer app__flex-3'>
-        <div className='img-size footer-logo'>
+      <footer className={styles.footer}>
+        <div className={['img-size', styles.footer_logo].join(' ')}>
           <img src={Images.logo} alt='logo' />
         </div>
-        <ul>
-          <p>Quick Links</p>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Students</li>
-          <li>Parents</li>
-        </ul>
-        <ul>
-          <p>Help</p>
-          <li>FAQ</li>
-          <li>Contact Us</li>
-          <li>Terms and Condition</li>
-          <li>Privacy Policy</li>
-        </ul>
-        <div className='footer__icons'>
-          <p>Socials</p>
-          <div className='icons app__flex'>
+        <div className={styles.footerLinks}>
+          <div className={styles.quickLinks}>
+            <h3>Quick Links</h3>
+            <ul>
+              {quickLinks.map((item, index) => (
+                <li key={index}>
+                  <Link to={item.path}>{item.name}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className={styles.footerHelp}>
+            <h3>Help</h3>
+            <ul>
+              {help.map((item, index) => (
+                <li key={index}>
+                  <Link to={item.path}>{item.name}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+
+        <div className={styles.footer__icons}>
+          <h3>Socials</h3>
+          <div className={styles.icons}>
             <BsFacebook />
             <AiFillTwitterCircle />
             <AiFillInstagram />
@@ -42,7 +85,9 @@ const Footer = () => {
         </div>
       </footer>
       <hr />
-      <h6>© 2023 Link-Park. All Rights Reserved</h6>
+      <h6 className={styles.footer_end}>
+        © 2023 Link-Park. All Rights Reserved
+      </h6>
     </div>
   )
 }
