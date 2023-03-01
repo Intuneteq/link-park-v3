e4d93a5862b74d5582bc8ed3524db0e6cc782bdc
Tobi Olanitori <tobiolanitori@gmail.com>
2023-03-01 04:37:05 +0100

    feat: calendar initial setup

--- src/components/organisms/calendar/index.jsx
+++ src/components/organisms/calendar/index.jsx
@@ -1,14 +1,32 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import dayjs from 'dayjs'
+import TextField from '@mui/material/TextField'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
+import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker'
+
 import './calendar.scss'
 
 const LinkParkCalendar = ({ showTitle, height }) => {
+  const [value, setValue] = React.useState(dayjs(Date.now()))
   return (
     <div className='dashboard-calender'>
       {showTitle ? '' : <h3>Calender</h3>}
-      <div style={{ height }} className='calendar'>
-        okay
+      <div style={{ height }}>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <StaticDatePicker
+            className='calendar__'
+            displayStaticWrapperAs='desktop'
+            openTo='day'
+            value={value}
+            onChange={(newValue) => {
+              setValue(newValue)
+            }}
+            renderInput={(params) => <TextField {...params} />}
+          />
+        </LocalizationProvider>
       </div>
     </div>
   )
