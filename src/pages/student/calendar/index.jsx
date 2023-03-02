f79b169c54de40a606fc8fbaf6555df8d643f2fc
Tobi Olanitori <tobiolanitori@gmail.com>
2023-03-02 05:21:21 +0100

    feat: dashboard calendars done

--- src/pages/student/calendar/index.jsx
+++ src/pages/student/calendar/index.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import { IoIosArrowForward, IoIosArrowBack } from 'react-icons/io'
 
+import { useGetScreenSize } from '../../../hooks/useMediaQuery'
 import { LinkParkCalendar } from '../../../components/organisms'
 import { Container } from '../../../components/templates'
 import { Images } from '../../../constants'
 import './calendar.scss'
 
 const StudentCalendar = () => {
+  const { isMobile } = useGetScreenSize()
   return (
     <Container name={'Calendar'}>
       <section className='student_calendar'>
         <div className='calendar_body'>
-          <LinkParkCalendar className='calendar__' />
+          <LinkParkCalendar className={!isMobile && 'calendar__'} />
         </div>
         <div className='calendar_facts'>
           <h3>Fun Fact On This Day</h3>
