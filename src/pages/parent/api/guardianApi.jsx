ab0c1647194ebbcb008da81efa7467c74797d69a
Intuneteq <tobiolanitori@gmail.com>
2023-05-19 00:12:43 +0100

    chore: code clean up

--- src/pages/parent/api/guardianApi.jsx
+++ src/pages/parent/api/guardianApi.jsx
@@ -1,31 +1,4 @@
 import { apiSlice } from '../../../app/api'
-// import { setGuardianStudents } from './guardianSlice'
-
-// export const guardianApiSlice = apiSlice.injectEndpoints({
-//   endpoints: (builder) => ({
-//     getStudentsByGuardianId: builder.mutation({
-//       query: (guardianID) => ({
-//         url: `/user/guardian/${guardianID}`,
-//         method: 'GET',
-//       }),
-//       transformResponse: (res) => {
-//         console.log('success fetch', res)
-//         return res
-//       },
-//       // async onQueryStarted(arg, { dispatch, queryFulfilled }) {
-//       //   try {
-//       //     const { data } = await queryFulfilled
-//       //     console.log('success fetch', data)
-//       //     // dispatch(setGuardianStudents(data.data))
-//       //   } catch (err) {
-//       //     console.log(err)
-//       //   }
-//       // },
-//     }),
-//   }),
-// })
-
-// export const { useGetStudentsByGuardianIdMutation } = guardianApiSlice
 
 export const guardianApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
