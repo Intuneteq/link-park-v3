eca5d617ba79d9a92f203e23f434970344242148
Intuneteq <tobiolanitori@gmail.com>
2023-05-23 14:49:15 +0100

    refactor: Took off redux-persist

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,45 +1,19 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
-import storage from 'redux-persist/lib/storage'
-import {
-  persistReducer,
-  persistStore,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist'
-import thunk from 'redux-thunk'
 
 // Reducers
 import { apiSlice } from './api/apiSlice'
 import authReducer from '../pages/auth/api/authSlice'
 import guardianReducer from '../pages/parent/api/guardianSlice'
 
-const persistConfig = {
-  key: 'root',
-  storage,
-  version: 1,
-  blackList: [],
-}
-
 const rootReducer = combineReducers({
   [apiSlice.reducerPath]: apiSlice.reducer,
   auth: authReducer,
   guardian: guardianReducer,
 })
-const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-  reducer: persistedReducer,
+  reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }).concat(thunk, apiSlice.middleware),
+    getDefaultMiddleware().concat(apiSlice.middleware),
   devTools: process.env.NODE_ENV !== 'production',
 })
-
-export const persistor = persistStore(store)
--- src/index.jsx
+++ src/index.jsx
@@ -2,26 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react'
 import { NextUIProvider } from '@nextui-org/react'
 
 import App from './App'
-import { store, persistor } from './app/store'
+import { store } from './app/store'
 import './index.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <NextUIProvider disableBaseline={true}>
-          <BrowserRouter>
-            <Routes>
-              <Route path='/*' element={<App />} />
-            </Routes>
-          </BrowserRouter>
-        </NextUIProvider>
-      </PersistGate>
+      <NextUIProvider disableBaseline={true}>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/*' element={<App />} />
+          </Routes>
+        </BrowserRouter>
+      </NextUIProvider>
     </Provider>
   </React.StrictMode>
 )
